45700572152a085daa5bfaf600ebed01d9fcf69d
Babu Srikanthan <sudhsrik@umich.edu>
2022-11-09 18:31:49 -0500

    Initial commit

---
+++ js/jsChallenges.js
@@ -0,0 +1,32 @@
+// Challenge 4
+// Update the checkForm() function so that it checks whether or not you
+// have filled out the name and address fields. If not, display the appropriate
+// error message.
+
+// Extra challenge: Update the function so that if you fill out the field and
+// hit the button again, the error message is hidden again.
+
+function checkForm() {
+
+}
+
+
+// Challenge 5
+// Update the checkNum() function so that it checks whether or not you
+// have filled out the field using a number. If not, display the error message.
+// Then update the function to hide the error message if a number was entered.
+// HINT: look up the isNaN() function!
+
+function checkNum() {
+
+}
+
+
+// Challenge 6
+// We know how to use pseudo classes to change styling when we hover over something.
+// Try using JavaScript to do the same thing. When you hover over an image, display
+// the alt text.
+
+function displayText(image) {
+
+}
